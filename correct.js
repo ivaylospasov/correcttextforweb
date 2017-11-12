813f59005f3c95121a013a1bbd8b5855f094faed
ivaylospasov <ivaylo.pif@gmail.com>
2017-11-12 07:15:18 +0200

    add all files so far

---
+++ correct.js
@@ -0,0 +1,28 @@
+function fix() {
+    "use strict";
+    /*jslint browser:true */
+    var getForm, content, corrected, firstCode, i, myCode;
+    getForm = document.getElementById("formcorrect");
+    content = getForm.elements["textraw"].value;
+    corrected = content.replace(/(?:\r\n|\r|\n)/g, '<br>');
+    corrected = corrected.replace(/¤/g, "");
+    corrected = corrected.replace(/\./g, ". ");
+    corrected = corrected.replace(/,/g, ", ");
+    for (i = 0; i < 2; i += 1) {
+        corrected = corrected.replace(/ {2}/g, " ");
+    }
+    corrected = corrected.replace(/ ,/g, ", ");
+    corrected = corrected.replace(/ \./g, ". ");
+    corrected = corrected.replace(/ \:/g, ": ");
+    corrected = corrected.replace(/„/g, "\"");
+    corrected = corrected.replace(/“/g, "\"");
+    for (i = 0; i < 2; i += 1) {
+        corrected = corrected.replace(/ {2}/g, " ");
+    }
+    for (i = 30; i > 0; i -= 1) {
+        firstCode = "W" + i + ".*}";
+        myCode = new RegExp(firstCode);
+        corrected = corrected.replace(myCode, "");
+    }
+    document.getElementById("demo").innerHTML = corrected;
+}
